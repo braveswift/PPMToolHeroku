fc775e2ce0236a38b65f143aa143a83a667f2e0c
Yan Hong <yanhong@MacBook-Pro.local>
2020-08-10 17:24:51 -0700

    react

---
+++ src/main/resources/static/precache-manifest.b9ac24065cc3fe7a56ca7e1129a34e9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05628a1a485ef9b096bab5f943a98a51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02fb494b28ad3019927c",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7f438d4832305730c498",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "02fb494b28ad3019927c",
+    "url": "/static/js/2.5017abdd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5017abdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f438d4832305730c498",
+    "url": "/static/js/main.b0fceda9.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
